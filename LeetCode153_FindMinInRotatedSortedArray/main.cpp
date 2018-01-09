e67c5a01e63180ef0e7e524b196753139dc113b8
Rui <rui@dhcp35.merusouth117.iit.edu>
2018-01-08 23:17:07 -0600

    first commit

--- LeetCode153_FindMinInRotatedSortedArray/main.cpp
+++ LeetCode153_FindMinInRotatedSortedArray/main.cpp
@@ -13,3 +13,27 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+        if(nums[left] <= nums[right]) // if the nums[left] < nums[right} which means the array is not rotated.
+            return nums[left];
+        
+        while(left != (right - 1)) //do the loop if there is still numbers between the left and the right
+        {
+            int mid = (right + left) / 2; //create a pointer which is in the half of the left and the right
+            if(nums[mid] < nums[right]) // if the nums[mid] < nums[right], which means the min number is in the left part
+                right = mid; // make the right be the mid, and search the left part.
+            else
+                left = mid;//or do search the right part
+        }
+        
+        return min(nums[left], nums[right]);
+        
+    }
+};
